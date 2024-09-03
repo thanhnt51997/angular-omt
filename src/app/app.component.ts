6123e6902e698c0bc4c926fefc4d500817a5104d
hoangln <hoang.ln@evotek.vn>
2024-09-03 10:50:13 +0700

    blank project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {UserModel} from "./models/user.model";
+import * as jsonCountries from '../assets/countries.json';
+import * as uuid from 'uuid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  contact = {
-    "contactName": "",
-    "email": "abc@deg.com",
-    "facebook": "facebook.com",
-    "twitter": "twitter.com",
-    "website": "tiepphan.com",
-    "tel": "1234-5678-90"
-  }
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  onSubmit(formValue: any) {
-    // Do something awesome
-    //console.log(formValue);
-    // throw Error('something go wrong')
-  }
+export class AppComponent{
 }
