46c85b4e62504b63475b6b8d330a495196d15e02
Vinh <vinhhttt@gmail.com>
2024-09-05 23:16:28 +0700

    form user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,75 @@
 import {Component} from '@angular/core';
 import {UserModel} from "./models/user.model";
+import {Country} from "./models/country.model";
 import * as jsonCountries from '../assets/countries.json';
-import * as uuid from 'uuid';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent{
-  title() {
+export class AppComponent {
+    title() {
 
-  }
+    }
+
+
+    userLists: UserModel[] = [];
+    listSkills :string[] =['C#', 'PHP', 'Angular']
+
+    countries: Country[] = (jsonCountries as any).default as Country[];
+
+    selectedSkills: boolean[] = [];
+    user: UserModel = {
+        name: '',
+        email: '',
+        gender: '',
+        phone: '',
+        country: '',
+        address: '',
+        skill: '',
+        description: ''
+    };
+    editingIndex: number | null = null;
+
+    onSubmit() {
+        if (this.editingIndex !== null) {
+            this.userLists[this.editingIndex] = { ...this.user };
+            this.editingIndex = null; // Clear editing index
+        } else {
+            this.userLists.push({ ...this.user });
+        }
+        this.resetForm();
+    }
+
+    deleteUser(index: number) {
+        this.userLists.splice(index, 1);
+    }
+
+    editUser(index: number) {
+        this.user = { ...this.userLists[index] };
+        this.editingIndex = index;
+        this.selectedSkills = this.listSkills.map(skill => this.user.skill.includes(skill));
+    }
+
+    resetForm() {
+        this.user = {
+            name: '',
+            email: '',
+            gender: '',
+            phone: '',
+            country: '',
+            address: '',
+            skill: '',
+            description: ''
+        };
+        this.selectedSkills = [];
+    }
+
+    onSkillChange(index: number) {
+        this.user.skill = this.listSkills
+            .filter((_, i) => this.selectedSkills[i])
+            .join(', ');
+    }
 }
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,2 +1,10 @@
-export interface UserModel{
+export interface UserModel {
+    name: string;
+    email: string;
+    gender: string;
+    phone: string;
+    country: string;
+    address: string;
+    skill: string;
+    description: string;
 }
