817614784b3ec4d31289014cf34af7b6b07f4ba5
VinhXD <vinhhttt@gmail.com>
2024-09-08 21:18:48 +0700

    edit user-form by create component and add logic change color when select gender

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import {Component} from '@angular/core';
-import {UserModel} from "./models/user.model";
-import {Country} from "./models/country.model";
-import * as jsonCountries from '../assets/countries.json';
 
 
 @Component({
@@ -10,67 +7,6 @@ import * as jsonCountries from '../assets/countries.json';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title() {
-
-    }
 
 
-    userLists: UserModel[] = [];
-    listSkills :string[] =['C#', 'PHP', 'Angular']
-
-    countries: Country[] = (jsonCountries as any).default as Country[];
-
-    selectedSkills: boolean[] = [];
-    user: UserModel = {
-        name: '',
-        email: '',
-        gender: '',
-        phone: '',
-        country: '',
-        address: '',
-        skill: '',
-        description: ''
-    };
-    editingIndex: number | null = null;
-
-    onSubmit() {
-        if (this.editingIndex !== null) {
-            this.userLists[this.editingIndex] = { ...this.user };
-            this.editingIndex = null;
-        } else {
-            this.userLists.push({ ...this.user });
-        }
-        this.resetForm();
-    }
-
-    deleteUser(index: number) {
-        this.userLists.splice(index, 1);
-        this.resetForm();
-    }
-
-    editUser(index: number) {
-        this.user = { ...this.userLists[index] };
-        this.editingIndex = index;
-        this.selectedSkills = this.listSkills.map(skill => this.user.skill.includes(skill));
-    }
-
-    resetForm() {
-        this.user = {
-            name: '',
-            email: '',
-            gender: '',
-            phone: '',
-            country: '',
-            address: '',
-            skill: '',
-            description: ''
-        };
-        this.selectedSkills = [];
-    }
-
-    onSkillChange(index: number) {
-        this.user.skill = this.listSkills
-            .filter((_, i) => this.selectedSkills[i])
-            .join(', ');
-    }
 }
